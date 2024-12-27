99670c7b867f2627805bf405554c119c7cb237a5
KoSchoemaker <155833082+KoSchoemaker@users.noreply.github.com>
2024-12-27 21:45:52 +0100

    widen (maybe too much, shall see)

--- Visualisation/js/rankchart.js
+++ Visualisation/js/rankchart.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+  width = 680 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3
